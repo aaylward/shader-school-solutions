6da5668f96f1b2013b3b0f46055c9d9147b8e029
Andy Aylward <aaylward@gmail.com>
2015-06-07 23:58:02 -0400

    first commit

---
+++ 02-intro-2/sides.glsl
@@ -0,0 +1,14 @@
+void sideLengths(
+  highp float hypotenuse, 
+  highp float angleInDegrees, 
+  out highp float opposite, 
+  out highp float adjacent) {
+
+  highp float angleInRadians = radians(angleInDegrees);
+  opposite = sin(angleInRadians) * hypotenuse;
+  adjacent = cos(angleInRadians) * hypotenuse;
+  
+}
+
+//Do not change this line
+#pragma glslify: export(sideLengths)
