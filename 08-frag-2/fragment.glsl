24003756f4bfea758e428290876a44fcd4108f70
Andy Aylward <aaylward@gmail.com>
2015-06-08 23:41:26 -0400

    checkerboard :checkered_flag:

--- 08-frag-2/fragment.glsl
+++ 08-frag-2/fragment.glsl
@@ -1,8 +1,17 @@
 precision mediump float;
 
-void main() {
+#define WHITE        vec4(1.0)
+#define GRID_SIZE    16.0
 
-  //TODO: Replace this with a function that draws a checkerboard
-  
-  discard;
+bool inTile(vec2 p, float tileSize) {
+  vec2 ptile = step(0.5, fract(0.5 * p / tileSize));
+  return ptile.x == ptile.y;
+}
+
+void main() {
+  if (inTile(gl_FragCoord.xy, GRID_SIZE)) {
+    gl_FragColor = WHITE;
+  } else {
+    discard;
+  }
 }
