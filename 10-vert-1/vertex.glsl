ae88d58cd171c23ef575e0d9f629f4343f3f06d6
Andy Aylward <aaylward@gmail.com>
2015-06-10 01:31:51 -0400

    this doesn't quite work...

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -4,9 +4,18 @@ uniform float theta;
 
 attribute vec2 position;
 
+// x = r cos(ø)
+// y = r sin(ø)
+
+// r = sqrt(potions.x * position.x + position.y * position.y)
+// ø = arctan(position.y / position.x)
+
+// position = vec4(r * cos(ø + theta), r * sin(ø + theta), 0, 1.0)
+
 void main() {
+  // rotate position by theta radians about the origin
+  float r = sqrt(pow(position.x, 2.0) + pow(position.y, 2.0));
+  float angle = atan(position.y / position.x) + theta;
 
-  //TODO: rotate position by theta radians about the origin
-
-  gl_Position = vec4(position, 0, 1.0);
+  gl_Position = vec4(r*cos(angle), r*sin(angle), 0, 1.0);
 }
