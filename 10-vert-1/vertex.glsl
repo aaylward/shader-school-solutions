0b4d31c7cc15b67958748c28e47d81b0ae75ca57
Andy Aylward <aaylward@gmail.com>
2015-06-10 23:15:34 -0400

    this works better :rotating_light:

--- 10-vert-1/vertex.glsl
+++ 10-vert-1/vertex.glsl
@@ -4,18 +4,11 @@ uniform float theta;
 
 attribute vec2 position;
 
-// x = r cos(ø)
-// y = r sin(ø)
-
-// r = sqrt(potions.x * position.x + position.y * position.y)
-// ø = arctan(position.y / position.x)
-
-// position = vec4(r * cos(ø + theta), r * sin(ø + theta), 0, 1.0)
-
 void main() {
   // rotate position by theta radians about the origin
-  float r = sqrt(pow(position.x, 2.0) + pow(position.y, 2.0));
-  float angle = atan(position.y / position.x) + theta;
+  mat2 rotation;
+  rotation[0] = vec2(cos(theta), sin(theta));
+  rotation[1] = vec2(-sin(theta), cos(theta));
 
-  gl_Position = vec4(r*cos(angle), r*sin(angle), 0, 1.0);
+  gl_Position = vec4((rotation*position), 0, 1.0);
 }
